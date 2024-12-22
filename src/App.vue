580e9085ee258683e505f75c87f225b73d80d809
OybekovAsilbek <islombekoybekov2@gmail.com>
2024-12-22 21:31:40 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+     <h1>Hello word</h1>
+     <h2>Akam</h2>
+
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
