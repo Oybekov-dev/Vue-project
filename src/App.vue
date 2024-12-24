298d187ccb9224a3ab2a9414875b2df82e23742b
OybekovAsilbek <islombekoybekov2@gmail.com>
2024-12-24 21:12:17 +0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,9 @@
-<script setup>
-import HelloWorld from "./components/HelloWorld.vue";
-</script>
-
 <template>
   <div>
     <div class="app">
       <div class="container">
-        <div id="row" class="d-flex gap-4">
-          <div id="box1" class="w-50">
+        <div class="d-flex gap-4">
+          <div class="w-50">
             <div class="alert alert-danger text-center" v-if="error">
               {{ errorAlert }}
             </div>
@@ -15,10 +11,9 @@ import HelloWorld from "./components/HelloWorld.vue";
               <div class="mb-3">
                 <label for="name" class="form-label">Your name</label>
                 <input
-                  type="email"
+                  type="text"
                   class="form-control"
                   id="name"
-                  aria-describedby="emailHelp"
                   v-model="name"
                 />
               </div>
@@ -28,7 +23,6 @@ import HelloWorld from "./components/HelloWorld.vue";
                   type="email"
                   class="form-control"
                   id="email"
-                  aria-describedby="emailHelp"
                   v-model="email"
                 />
                 <div id="emailHelp" class="form-text">
@@ -49,7 +43,7 @@ import HelloWorld from "./components/HelloWorld.vue";
               </button>
             </form>
           </div>
-          <div id="box2" class="w-50">
+          <div class="w-50">
             <table class="table">
               <thead>
                 <tr>
@@ -62,7 +56,7 @@ import HelloWorld from "./components/HelloWorld.vue";
                 </tr>
               </thead>
               <tbody>
-                <tr v-for="user in revUsers">
+                <tr v-for="user in users" :key="user.id">
                   <th scope="row">{{ user.id }}</th>
                   <td>{{ user.name }}</td>
                   <td>{{ user.email }}</td>
@@ -100,18 +94,17 @@ export default {
       changeUser: null,
     };
   },
-
   methods: {
     addUser() {
       if (this.changeUser != null) {
-        let findUserById = this.users.find(
-          (user) => user.id == this.changeUser
-        );
+        let findUserById = this.users.find((user) => user.id == this.changeUser);
         findUserById.name = this.name;
         findUserById.email = this.email;
         findUserById.age = this.age;
         this.changeUser = null;
-        (this.name = ""), (this.email = ""), (this.age = "");
+        this.name = "";
+        this.email = "";
+        this.age = "";
       } else if (this.name && this.email && this.age) {
         let newUser = {
           id: this.users.length + 1,
@@ -120,10 +113,10 @@ export default {
           age: this.age,
         };
         this.users.push(newUser);
-        (this.name = ""),
-          (this.email = ""),
-          (this.age = ""),
-          (this.error = false);
+        this.name = "";
+        this.email = "";
+        this.age = "";
+        this.error = false;
       } else {
         this.error = true;
         this.errorAlert = "Iltimos bo'sh joyni to'ldring";
@@ -140,10 +133,6 @@ export default {
       this.changeUser = id;
     },
   },
-  computed: {
-    revUsers() {
-      return this.users.reverse();
-    },
-  },
 };
 </script>
+
